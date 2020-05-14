9b605432a33116b966253e4e3c7356d0c51c4099
PeiyunChen0131 <64571747+PeiyunChen0131@users.noreply.github.com>
2020-05-13 21:13:49 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,15 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => res.render('welcome'));
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
+
 module.exports = router;
